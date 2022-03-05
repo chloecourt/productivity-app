7c361d46ff7aecd343d9620df4aef43be7264bc7
Chloe Court <chloe.azria@gmail.com>
2022-03-05 16:14:46 -0500

    JobComponent potential throw away

--- src/client/BottomContainer/ListContainer.jsx
+++ src/client/BottomContainer/ListContainer.jsx
@@ -5,30 +5,40 @@ import Filter from './Filter';
 
 const ListContainer = () => {
   const [jobs, setJobs] = useState([]);
-  const getData = ()=> {
-    const fakeData = [{applicationId: 1, company: "google", role: "software engineer", url: "www.hello.com"}, {applicationId:0, company: "amazon", role: "senior software engineer", url: "www.yello.com"}]; 
-    setJobs(fakeData);
-    console.log('getData invoked!')
-  }
+   
 
+  // const getData = ()=> {
+
+  //   const fakeData = {applicationId: 1, company: "google", role: "software engineer", url: "www.hello.com"}; 
+
+  //   setJobs([...jobs, fakeData]);
+    
+  // }
+  
   useEffect(()=> {
-    getData();
+    // getData();
+    setJobs([...jobs,{applicationId:0, company: "amazon", role: "senior software engineer", url: "www.yello.com"}]);
+    
   },[]); 
+  
+  // need an array of JSX components 
+  
   
   // get request func for fetching the jobs from the database
   // deconstruct {application_id, company, role, url } =data 
-  const jobsObj = jobs.map((job)=> {
-      return <Job key={job.applicationId} props = {job}/>
-  });
-    // populate jobs with 
+  // const jobsArray = jobs.map((job)=> { 
+  //     return <Job key={job.applicationId} props = {job}/>
+  // });
+    // populate jobs with
+    const arrayJobs = [];
+    for (let i=0; i < jobs.length; i++) {
+      arrayJobs.push(<Job key={jobs[i].applicationId} jobsObj={jobs[i]}/>)
+    }
   return (
     <div>
     <Filter/>
     <Form/>
-
-
-    {/* unique key from application_id */}
-    {jobsObj}
+    {arrayJobs}
   </div>
   );
 }
---
+++ src/client/BottomContainer/Table.jsx
@@ -0,0 +1,20 @@
+// import React from "react";
+// import Job from "./Job"
+
+// const Table = () => {
+// // think we should have a status column? 
+//   return (
+//     <>
+//       <table>
+//           <tr className="formColumnNames">
+//           <th>Company</th>
+//           <th>role</th>
+//           <th>url</th>
+//           </tr>
+//           <Job/>
+//       </table>
+//     </>
+//   );
+// };
+
+// export default Table; 
